d4ab840c07a115049fb05100041152e35502a81d
kennedybabu <kennedynjogu8@gmail.com>
2022-06-11 19:04:28 +0300

    feature: fetch destination data from local json file

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import data from '../components/data/data.json'
 
 const Destinations = () => {
+    const destinations = data.destinations
+    console.log(destinations)
+
+   
   return (
     <div className='destinations w-full h-screen'>
 
