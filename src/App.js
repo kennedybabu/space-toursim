44e813c9eeb35d9b2053b90001661dcb5c91a3b5
kennedybabu <kennedynjogu8@gmail.com>
2022-06-12 11:11:00 +0300

    feature: creat the europa component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Route , Routes} from 'react-router-dom'
 import Destinations from "./pages/Destinations";
+import Europa from "./pages/Europa";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/destinations' element={<Destinations />} />
+        <Route path='/europa' element={<Europa />} />
       </Routes>
     </>
   );
--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import {data} from '../components/data/data'
+import { Link } from 'react-router-dom'
 
 const Destinations = () => {
     // store data from json file
     const destinations = data.destinations
     console.log(data.destinations)
 
-    // get random destination
-    const destination = destinations[Math.floor(Math.random() * destinations.length)]
+    // get a destination
+    // const destination = destinations[Math.floor(Math.random() * destinations.length)]
+    const destination =destinations[0]
     
     console.log(destination.images.png)
     
@@ -16,29 +18,29 @@ const Destinations = () => {
     <div className='destinations w-full h-screen text-white'>
         {/* container */}
         <div className='grid lg:grid-cols-2 h-full px-[24px] md:px-[96px]'>
-            <div className='h-full flex justify-end lg:justify-center items-center flex-col w-full'>
-                <p className='condensed uppercase font-bold'><span>01</span>Pick your destination</p>
-                <div className='h-[170px] w-[170px] mt-8'>
-                    <img className='h-full w-full' src={destination.images.png} alt="" />
+            <div className='h-full flex justify-center  lg:justify-center items-center flex-col w-full mt-[60px] lg:mt-0 '>
+                <p className='lg:mt-[80px] condensed uppercase font-bold lg:text-[24px]'><span className='text-gray-400 mr-2'>01</span>Pick your destination</p>
+                <div className='h-[170px] w-[170px] mt-[32px]  md:h-[300px] md:w-[300px] lg:h-[445px] lg:w-[445px] lg:mt-[40px]'>
+                    <img className='h-full w-full' src={require ('../assets/destination/image-moon.png')} alt="" />
                 </div>
             </div>
-            <div className='h-full flex justify-end lg:justify-center items-center flex-col pb-[62px]'>
+            <div className='h-full flex justify-end  lg:pb-[118px] items-center lg:items-start flex-col md:pb-[62px] lg:h-[445px] lg:absolute lg:right-[162px] lg:bottom-0 lg:justify-between'>
                 <div>
-                   links
+                   <li><Link to='/europa'>Europa</Link></li>
                 </div>
                 <h1 className='text-[50px] uppercase'>{destination.name}</h1>
-                <p className='text-center text-[#d0d6f9]'>{destination.description}</p>
+                <p className='text-center lg:text-left text-[#d0d6f9] lg:max-w-[472px]'>{destination.description}</p>
                 <div className='w-full h-[40px] mt-4 lg:hidden md:mt-[49px]'>
                     <hr />
                 </div>
-                <div className='flex flex-col md:flex-row w-full justify-between md:px-[100px]'>
-                    <div className='flex flex-col items-center'>
+                <div className='flex flex-col md:flex-row w-full justify-between md:px-[100px] lg:px-0 lg:justify-start lg:mt-8'>
+                    <div className='flex flex-col items-center lg:mr-8'>
                         <p className='uppercase condensed text-[#d0d6f9]'>Avg Distance</p>
-                        <p className='text-3xl'>384,400 KM</p>
+                        <p className='text-3xl'>{destination.distance}</p>
                     </div>
-                    <div className='flex flex-col items-center'>
+                    <div className='flex flex-col items-center mt-[32px] md:mt-0'>
                         <p className='uppercase condensed text-[#d0d6f9]'>Est Travel Time</p>
-                        <p className='text-3xl uppercase'>3 Days</p>
+                        <p className='text-3xl uppercase'>{destination.travel}</p>
                     </div>
                 </div>
             </div>
