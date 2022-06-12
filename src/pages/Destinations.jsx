580b7a9a20327b66c7f3c897bfc9e4e2efef0d65
kennedybabu <kennedynjogu8@gmail.com>
2022-06-12 13:01:27 +0300

    fix: fix issue with overlapping content in smaller devices

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -15,12 +15,13 @@ const Destinations = () => {
     
  
   return (
+   
     <div className='destinations w-full h-screen text-white'>
         {/* container */}
         <div className='grid lg:grid-cols-2 h-full px-[24px] md:px-[96px]'>
-            <div className='h-full flex justify-center  lg:justify-center items-center flex-col w-full mt-[60px] lg:mt-0 '>
+            <div className='h-full flex justify-center  lg:justify-center items-center flex-col w-full  mt-[40px] md:mt-[60px] lg:mt-0 '>
                 <p className='lg:mt-[80px] condensed uppercase font-bold lg:text-[24px]'><span className='text-gray-400 mr-2'>01</span>Pick your destination</p>
-                <div className='h-[170px] w-[170px] mt-[32px]  md:h-[300px] md:w-[300px] lg:h-[445px] lg:w-[445px] lg:mt-[40px]'>
+                <div className='h-[170px] w-[170px] mt-[10px]  md:h-[300px] md:w-[300px] lg:h-[445px] lg:w-[445px] lg:mt-[40px]'>
                     <img className='h-full w-full' src={require ('../assets/destination/image-moon.png')} alt="" />
                 </div>
             </div>
@@ -33,17 +34,17 @@ const Destinations = () => {
                 </div>
                 <h1 className='text-[50px] uppercase'>{destination.name}</h1>
                 <p className='text-center lg:text-left text-[#d0d6f9] lg:max-w-[472px]'>{destination.description}</p>
-                <div className='w-full h-[40px] mt-4 lg:hidden md:mt-[49px]'>
+                <div className='w-full h-[10px] mt-4 lg:hidden md:mt-[49px]'>
                     <hr />
                 </div>
                 <div className='flex flex-col md:flex-row w-full justify-between md:px-[100px] lg:px-0 lg:justify-start lg:mt-8'>
                     <div className='flex flex-col items-center lg:mr-8 lg:items-start'>
                         <p className='uppercase condensed text-[#d0d6f9]'>Avg Distance</p>
-                        <p className='text-3xl uppercase'>{destination.distance}</p>
+                        <p className='text-2xl md:text-3xl uppercase'>{destination.distance}</p>
                     </div>
-                    <div className='flex flex-col items-center mt-[32px] md:mt-0'>
+                    <div className='flex flex-col items-center mt-[10px] md:mt-0'>
                         <p className='uppercase condensed text-[#d0d6f9]'>Est Travel Time</p>
-                        <p className='text-3xl uppercase'>{destination.travel}</p>
+                        <p className='text-2xl md:text-3xl uppercase'>{destination.travel}</p>
                     </div>
                 </div>
             </div>
